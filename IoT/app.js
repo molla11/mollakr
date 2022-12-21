f012287a4e39968382bd23fd82233e1634ced043
molla <hsjang635@naver.com>
2022-12-21 12:16:01 +0900

    Update swipe

--- IoT/app.js
+++ IoT/app.js
@@ -1,22 +1,17 @@
-const left = document.querySelector("#left");
-const right = document.querySelector("#right");
-const img = document.querySelector("img");
-left.addEventListener('click', () => {
-    const idxOfimg = parseInt(img.src.slice(-5, img.src.length).substring(0, 1));
-    if (idxOfimg >= 2) {
-        changeImage(-1);
-    }
-    else if (idxOfimg === 0) {
-        changeImage(9);
-    }
+const mySwiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  autoplay: {
+    delay: 10000,
+  },
 });
-right.addEventListener('click', () => {
-    const idxOfimg = parseInt(img.src.slice(-5, img.src.length).substring(0, 1));
-    if (idxOfimg <= 9 && !(idxOfimg === 0)) {
-        changeImage(1);
-    }
-});
-function changeImage(amount) {
-    const idxOfimg = parseInt(img.src.slice(-5, img.src.length).substring(0, 1));
-    img.src = `img/${idxOfimg + amount}.png`;
-}
