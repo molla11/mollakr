919b2511a7e52efb44ebfee82755b061892c2ff3
molla <hsjang635@naver.com>
2022-12-22 14:55:40 +0900

    Update app.js

--- IoT/app.js
+++ IoT/app.js
@@ -12,3 +12,46 @@ const mySwiper = new Swiper('.swiper-container', {
     draggable: true,
   },
 });
+
+const buttonNext = document.querySelector('.swiper-button-next');
+const buttonPrev = document.querySelector('.swiper-button-prev');
+
+window.onkeydown = (e) => {
+  console.log(e);
+  switch (e.key) {
+    case 'ArrowLeft':
+      buttonPrev.click();
+      break;
+
+    case 'ArrowRight':
+      buttonNext.click();
+      break;
+
+    case ' ':
+      buttonNext.click();
+      break;
+
+    case 'Enter':
+      buttonNext.click();
+      break;
+    
+    case 'PageUp':
+      buttonPrev.click();
+      break;
+
+    case 'PageDown':
+      buttonNext.click();
+      break;
+    
+    case 'Home':
+      for (let i = 0; i < 10; i++) { buttonPrev.click(); }
+      break;
+    
+    case 'End':
+      for (let i = 0; i < 10; i++) { buttonNext.click(); }
+      break;
+
+    default:
+      break;
+  }
+}
